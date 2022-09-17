7004ff2b6c605386800a2e9bf76d1c343309cc1b
SurajMegharaj <87960243+SurajMegharaj@users.noreply.github.com>
2022-09-17 09:07:41 +0530

    Create index.py

---
+++ index.py
@@ -0,0 +1,16 @@
+# Develop a MADLIBS Game
+
+# About  Madlibs
+
+#  Take the input from user and add the words to the pre-formed sentences
+
+#Algorithm
+# Step 1 : Declare a variable get the input to be filled in the paragraph
+# Step2:  Using format method or you can literally use the simple concatination to add up to the paragraph
+
+var1 = input("Name :")
+var2 = input("age :")
+var3 = input("language :")
+
+print("Hey hi, This is {} , {} years old . I really want to Learn {}, Where should i learn it from ?".format(var1,var2,var3))
+print(" Just Follow Learnwith Surajmegharaj")
